e7be1e8ee6cd38ec1d77a240608f61bbbdb7d469
Ed Bueler <elbueler@alaska.edu>
2019-02-03 09:26:39 -0900

    work on exercises in slides

---
+++ assets/codes/uscensus.m
@@ -0,0 +1,12 @@
+% USCENSUS  Table of data from page 102 of Zill 11th edition (exercise 4 in
+% section 3.2).  Associated plot.
+
+year = 1790:10:1950;    % list of 17 values
+pop = [  3.929,   5.308,   7.240,   9.638,  12.866, ...
+        17.069,  23.192,  31.433,  38.558,  50.156, ...
+        62.948,  75.996,  91.972, 105.711, 122.775, ...
+       131.669, 150.697];
+
+plot(year, pop, '.k', 'markersize', 14)
+xlabel('census year'),  ylabel('population in millions')
+axis([1780 1960 0 160]),  grid on
